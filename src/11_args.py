17478b01c9373bc007852a1d9b8325d752c24121
Cai Nowicki <cai.nowicki@gmail.com>
2020-09-03 00:59:14 -0400

    finished exercise 11

--- src/11_args.py
+++ src/11_args.py
@@ -17,7 +17,7 @@ def f2(*numbers):
     sum = 0
     for number in numbers:
         sum += number
-    return number
+    return sum
 
 
 print(f2(1))                    # Should print 1
@@ -28,14 +28,16 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(sum([f2(x) for x in a]))    # Should print 22
+# I don't think this is what they wanted but it gives the right answer!
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+def f3(a, b=1):
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -49,7 +51,11 @@ print(f3(8))     # Should print 9
 #
 # Note: Google "python keyword arguments".
 
-# YOUR CODE HERE
+def f4(**kwargs):
+    for k,v in kwargs.items():
+        print(f"key: {k}, value: {v}")
+
+
 
 # Should print
 # key: a, value: 12
@@ -68,4 +74,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
